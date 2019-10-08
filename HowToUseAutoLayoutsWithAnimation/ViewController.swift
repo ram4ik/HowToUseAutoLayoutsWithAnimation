d1d2cf642d1ea455226509486147dab963fe8f54
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-10-08 13:02:26 +0300

    init

--- HowToUseAutoLayoutsWithAnimation/ViewController.swift
+++ HowToUseAutoLayoutsWithAnimation/ViewController.swift
@@ -13,6 +13,29 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "This is something..."
+        label.font = UIFont.systemFont(ofSize: 25, weight: .bold)
+        view.addSubview(label)
+        
+        label.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        label.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
+        var xAnchor = label.centerXAnchor.constraint(equalTo: view.rightAnchor, constant:  200)
+        xAnchor.isActive = true
+        
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 1) {
+            xAnchor.isActive = false
+            
+            xAnchor = label.centerXAnchor.constraint(equalTo: self.view.centerXAnchor)
+            xAnchor.isActive = true
+            
+            UIView.animate(withDuration: 0.5, delay: 1, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options: .curveEaseIn, animations: {
+                self.view.layoutIfNeeded()
+            })
+        }
     }
 
 
